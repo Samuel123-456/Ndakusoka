ffc1002d90d6ce078c91af4eb59c941da6038f77
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-05-03 15:29:22 +0100

    created a socialmedia models

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from web.models import SocialMedia
 
 # Register your models here.
+admin.site.register(SocialMedia)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class SocialMedia(models.Model):
+      """
+      Model SocialMedia
+            def: Refere-se a links para redes sociais dos usuarios 
+
+      Atributos
+            user (User): O usuario desta rede social
+            social_media (str): Redes Socias disponiveis
+            link (url): link para a rede social do usuario
+      """
+      SM_CHOICES = [
+            ('Wh', 'WhatsApp'),
+            ('Fb', 'FaceBook'),
+            ('LI', 'LinkedIn'),
+      ]
+      user = models.ForeignKey(User, models.CASCADE)
+      social_media = models.CharField(verbose_name='Redes Sociais', choices=SM_CHOICES)
+      link = models.URLField(verbose_name='Link da rede social', unique=True)
