687f7a371a8f7a76f76c810cb3f2e29a7989a8a1
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-05-05 14:45:14 +0100

    create a js for message closing button

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,19 +1,6 @@
+var btn_close_messages = document.getElementById('btn-close-messages')
+var div_messages = document.getElementById('div-messages')
 
-let message = document.getElementById('div-message')
-
-let cont = 0
-
-let control = null
-function showMessage(params) {
-      
-      control = setInterval(() => {
-            if (cont==3) {
-                  message.style.display = 'none'
-                  clearInterval(control)
-            }
-            cont+=1
-      }, 1000);
-      message.style.display = 'flex'
-}
-
-showMessage()  
+btn_close_messages.addEventListener('click', ()=>{
+      div_messages.style.display = 'none'
+})
--- assets/mail/contact.js
+++ assets/mail/contact.js
@@ -11,6 +11,10 @@ $(function () {
             var subject = $("input#subject").val();
             var message = $("textarea#message").val();
 
+            var dj_message = $("#dj_message").val();
+
+
+
             $this = $("#sendMessageButton");
             $this.prop("disabled", true);
 
@@ -21,7 +25,8 @@ $(function () {
                     name: name,
                     email: email,
                     subject: subject,
-                    message: message
+                    message: message,
+                    dj_message: dj_message
                 },
                 cache: false,
                 success: function () {
@@ -38,7 +43,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + dj_message + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
