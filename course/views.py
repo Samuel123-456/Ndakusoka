af0888f7dbceced8ded920f1ea40c7e80b30e7a5
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-05-19 19:36:38 +0100

    create views for comment and remove comment

--- course/views.py
+++ course/views.py
@@ -4,7 +4,7 @@ from course.models import (
       Module,
       Comment
 )
-from django.views import View
+from django.contrib import messages
 
 # Create your views here.
 def viewCourses(request):
@@ -34,6 +34,60 @@ def viewCourseSingle(request, slug):
       ctx['course'] = course
       ctx['modules'] = modules
       ctx['comments'] = commets.order_by('date_commented')
-    
       
-      return render(request, template_name, ctx)
+      template_render = render(request, template_name, ctx)
+      template_render.set_cookie('course', slug)
+      
+      return template_render
+
+def comment(request):
+      if request.method == 'POST':
+            cookies = request.COOKIES
+            if 'course' not in cookies:
+                  return redirect('home')
+            
+            course = Course.objects.get(slug=cookies['course'])
+            user = request.user
+
+            url = f'/course/course-single/{course.slug}'
+
+            text = request.POST.get('text')
+
+            comment = Comment(
+                  author=user,
+                  course=course,
+                  text=text
+            )
+
+            user = comment.get_author_by_user()
+            if not user:
+                  messages.add_message(request, messages.ERROR, 'Somente Estudantes/Professores podem comentar')
+                  return redirect(url)
+
+            comment.save()
+            return redirect(url)
+
+def remove_comment(request, id):
+      comment = Comment.objects.filter(id=id)
+      cookies = request.COOKIES
+      if 'course' not in cookies:
+            return redirect('home')
+      
+      course = Course.objects.get(slug=cookies['course'])
+      url = f'/course/course-single/{course.slug}'
+
+
+      if not comment.exists():
+            messages.add_message(request, messages.ERROR, 'Comentario Nao existe')
+            return redirect(url)
+      comment = comment.first()
+
+      if comment.author != request.user:
+            messages.add_message(request, messages.ERROR, 'Comentario Nao pertence a esse usuario')
+            return redirect(url)
+
+
+      comment.is_active = False
+      comment.save()
+      
+      return redirect(url)
