efbaecc34bff509e87a1036c477cd2af33e779e3
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-05-19 19:35:25 +0100

    delete a comment from the course

--- course/urls.py
+++ course/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from course.views import (
       viewCourses,
       viewCourseSingle,
+      comment,
+      remove_comment
 )
 
 urlpatterns = [
       path('', viewCourses, name='course'),
+      path('comment/', comment, name='comment'),
+      path('remove/<int:id>', remove_comment, name='remove-comment'),
       path('course-single/<slug:slug>', viewCourseSingle, name='course_single')
 ]
