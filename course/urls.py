8dd3c216f94c5e423a854062ac99e6a5502f298c
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-06-01 21:57:27 +0100

    add watchCourse url

--- course/urls.py
+++ course/urls.py
@@ -3,12 +3,15 @@ from course.views import (
       viewCourses,
       viewCourseSingle,
       comment,
-      remove_comment
+      remove_comment,
+      watchCourse
 )
+
 
 urlpatterns = [
       path('', viewCourses, name='course'),
       path('comment/', comment, name='comment'),
       path('remove/<int:id>', remove_comment, name='remove-comment'),
-      path('course-single/<slug:slug>', viewCourseSingle, name='course_single')
+      path('course-single/<slug:slug>', viewCourseSingle, name='course_single'),
+      path('course-watch/<slug:slug>', watchCourse, name='watch')
 ]
