e3fc5cdb7f775a55969c131eae62bc4b9fa0e826
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-06-01 16:40:56 +0100

    add PaymentProof to the admin registration model

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from student.models import Student, Enrollment
+from student.models import Student, Enrollment, PaymentProof
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Enrollment)
+admin.site.register(PaymentProof)
