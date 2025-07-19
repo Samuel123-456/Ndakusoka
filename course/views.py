8ce9f15f6a9bb45b41acfca7952bde2d1d899ba7
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-07-19 10:45:33 +0100

    modify FBV to CBV for Course detai, list and aldo create a class View to handle comments

--- course/views.py
+++ course/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from course.models import (
       Course, 
       Module,
@@ -9,83 +9,78 @@ from student.models import Enrollment
 from django.contrib import messages
 from django.urls import reverse
 from django.db.models import Q
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from typing import Any
+ 
 
 # Create your views here.
-def viewCourses(request):
-      if request.method == 'GET':
-            template_name = 'course/course.html'
-            ctx = {}
-
-            ctx['courses'] = Course.objects.all()
-
-            my_courses = list()
-            if request.user.is_authenticated:
-                  enrollments = Enrollment.objects.filter(Q(student__user=request.user))
-                  my_courses = [ enrollment.course for enrollment in enrollments ]
-            ctx['my_courses'] = my_courses
-
-            return render(request, template_name, ctx)
-
-def viewCourseSingle(request, slug):
-      template_name = 'course/single.html'
-      ctx = {}
-
-      course = Course.objects.filter(slug=slug)
-      if not course.exists():
-            return redirect('course')
       
-      course = course.first()
-      modules = Module.objects.filter(course=course)
-      commets = Comment.objects.filter(course=course)
+class CourseListView(ListView):
 
-      student = None
-      if request.user.is_authenticated:
-            student = Enrollment.objects.filter(Q(student__user=request.user) & Q(course=course))
+      """CourseListView: List all courses including courses the user is included into."""
 
-      ctx['course'] = course
-      ctx['modules'] = modules
-      ctx['comments'] = commets.order_by('date_commented')
-      ctx['student'] = student
-      
-      template_render = render(request, template_name, ctx)
-      template_render.set_cookie('course', slug)
-      
-      return template_render
+      model = Course
+      template_name = 'course/course.html'
+      context_object_name = 'courses'
+      paginate_by = 3
 
-def comment(request):
-      if request.method == 'POST':
-            cookies = request.COOKIES
-            if 'course' not in cookies:
-                  return redirect('home')
+
+      def get_context_data(self, **kwargs) -> dict[str, Any]:
+            context = super().get_context_data(**kwargs)
+
+            if self.request.user.is_authenticated:
+                  user_enrollments = Enrollment.objects.filter(Q(student__user=self.request.user))
+                  context['my_courses'] = [ enrollment.course for enrollment in user_enrollments ]
             
-            course = Course.objects.get(slug=cookies['course'])
-            text = request.POST.get('text')
-
-            Comment(author=request.user,course=course,text=text).save()
-
-            return redirect(reverse('course_single', kwargs={'slug': course.slug}))
-
-def remove_comment(request, id):
-      comment = Comment.objects.filter(id=id)
-      cookies = request.COOKIES
-      template_name = redirect(reverse("course_single", kwargs={'slug': cookies['course']}))
-
-      if not comment.exists():
-            messages.add_message(request, messages.ERROR, 'Comentario Nao existe')
-            return template_name
-      comment = comment.first()
-
-      if comment.author != request.user:
-            messages.add_message(request, messages.ERROR, 'Comentario Nao pertence a esse usuario')
-            return template_name
-      if 'course' not in cookies:
-            return redirect('home')
-
-
-      comment.is_active = False
-      comment.save()
+            return context 
       
-      return template_name
+
+class CourseDetailView(DetailView):
+      model = Course
+      template_name = 'course/detail.html'
+
+      def get_context_data(self, **kwargs) -> dict[str, Any]:
+            context = super().get_context_data(**kwargs)
+            course__slug = kwargs['object'].slug
+
+            context["modules"] = Module.objects.filter(course__slug=course__slug)
+            context["comments"] = Comment.objects.filter(course__slug=course__slug).order_by('date_commented').reverse()
+            
+            if self.request.user.is_authenticated:
+                  context["is_student"] = Enrollment.objects.filter(Q(student__user=self.request.user) & Q(course__slug=course__slug))
+
+            return context
+      
+
+class CommentHandler:
+      """CommentHandler: Create and Delete course comments"""
+
+      @classmethod
+      def create_comment(self, request, slug: str):
+            url = reverse("course:detail", kwargs={"slug": slug})
+
+            course = Course.objects.filter(slug=slug)
+            if course.exists() == False: return redirect(url)
+            
+            text_comment = str(request.POST.get('text', None)).strip()
+            if not text_comment: return redirect(url)
+            
+
+            Comment.objects.create(author=request.user, course=course.first(), text=text_comment)
+            return redirect(url)
+      
+      
+      @classmethod
+      def delete_comment(self, request, id: str, slug: str):
+            comment = Comment.objects.filter(Q(author=request.user) & Q(id=id))
+            if comment.exists():
+                  comment.first().delete()
+            else:
+                  messages.add_message(request, messages.ERROR, "Comentario nao foi removido")
+            
+            return redirect(reverse("course:detail", kwargs={"slug": slug}))
+     
 
 def watchCourse(request, slug):
       template_name = 'course/watch.html'
