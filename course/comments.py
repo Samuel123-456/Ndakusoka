0fbf9a9f5e8b0577fbb4904689f8e547e1b5acdf
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-07-19 20:15:15 +0100

    create CommendHandle for the course comments

--- course/comments.py
+++ course/comments.py
@@ -1,16 +1,37 @@
-#TODO: so pode comentar quem for um usuario
+from django.shortcuts import redirect
+from course.models import (
+      Course, 
+      Comment,
+)
+from django.contrib import messages
+from django.urls import reverse
+from django.db.models import Q
+ 
 
-class Comments:
-      def __init__(self):
-            self.author = ...
-            self.comment = ...
-            self.comment_int = ...
+class CommentView:
       
-      def delete_comment(self):
-            pass
+      @classmethod
+      def create_comment(self, request, slug: str):
+            url = reverse("course:detail-course", kwargs={"slug": slug})
 
-      def send_comment(self):
-            pass
+            course = Course.objects.filter(slug=slug)
+            if course.exists() == False: return redirect(url)
+            
+            text_comment = str(request.POST.get('text', None)).strip()
+            if not text_comment: return redirect(url)
+            
 
-      def update(self):
-            pass
+            Comment.objects.create(author=request.user, course=course.first(), text=text_comment)
+            return redirect(url)
+      
+      
+      @classmethod
+      def delete_comment(self, request, id: str, slug: str):
+            comment = Comment.objects.filter(Q(author=request.user) & Q(id=id))
+            if comment.exists():
+                  comment.first().delete()
+            else:
+                  messages.add_message(request, messages.ERROR, "Comentario nao foi removido")
+            
+            return redirect(reverse("course:detail-course", kwargs={"slug": slug}))
+     
